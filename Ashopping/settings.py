dbd4868a3ca2a5321688d99726ea079cadffdb2c
Vj <vrutvikjani@gmail.com>
2023-03-06 18:31:53 +0530

    Edit Profile

--- Ashopping/settings.py
+++ Ashopping/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c(q1970)4@m&7d^t(-fz!rsx_@l$o9x1n@8$q05^#ohwmo)rso'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)  # True
 
 ALLOWED_HOSTS = []
 
@@ -43,17 +44,23 @@ INSTALLED_APPS = [
     'store',
     'carts',
     'orders',
+    'admin_honeypot',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django_session_timeout.middleware.SessionTimeoutMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+SESSION_EXPIRE_SECONDS = 3600
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+SESSION_TIMEOUT_REDIRECT = 'accounts/login'
 
 ROOT_URLCONF = 'Ashopping.urls'
 
@@ -150,8 +157,8 @@ MESSAGE_TAGS = {
 }
 
 #smtp configuration
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ashoppingdummy@gmail.com'
-EMAIL_HOST_PASSWORD = 'oxlvfdptvkxtbyqn'
-EMAIL_USE_TLS = True
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT',cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS',cast=bool)
--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404,redirect
-from .models import Product
+from .models import Product,ProductGallery
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id 
@@ -56,17 +56,23 @@ def product_detail(request,category_slug ,product_slug):
         
     else:
         orderproduct = None
+        
     # get the reviews
     reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
         
+    # get the product gallery
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+    
     context={
         'single_product':single_product,
         'in_cart':in_cart,
         'orderproduct':orderproduct,
         'reviews':reviews,
+        'product_gallery':product_gallery,
     }
     
     return render(request, 'store/product_detail.html',context)
+
 
 
 def search(request):
